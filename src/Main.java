75c12c99537c955e4addecbfcf9e8fc256d13147
Xujie Yuan <xyua0019@student.monash.edu>
2021-08-18 16:46:35 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String option = scanner.nextLine();
+        switch (option)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+
+        }
+        System.out.println("fk u");
+    }
+}

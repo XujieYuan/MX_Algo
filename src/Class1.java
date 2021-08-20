e963b48c0194b38342d2c99607a55af831c2e59b
Xujie Yuan <xyua0019@student.monash.edu>
2021-08-20 18:46:47 +0800

    2021.8.20

---
+++ src/Class1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Class1 {
+
+    private String option;
+
+    public Class1() {
+        option = "";
+    }
+
+    public Class1(String newOption) {
+        option = newOption;
+    }
+
+    public void display() {
+        System.out.println("Select a question:");
+        System.out.println("1. 打印出所有的水仙花数（一个三位数，其各位数字立方和等于该数本身。例：153=1*1*1+5*5*5+3*3*3）");
+        System.out.println(
+                "2.Leetcode169. Given an array nums of size n, return the majority element.\nThe majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.");
+        System.out.println("3.");
+        System.out.println("4.");
+        System.out.println("5.");
+
+    }
+
+    public void select() {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the class number:");
         String option = scanner.nextLine();
-        switch (option)
-        {
+        switch (option) {
             case "1":
+                Class1 c1 = new Class1();
+                c1.display();
                 break;
             case "2":
                 break;
-
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            case "8":
+                break;
+            case "9":
+                break;
+            case "10":
+                break;
+            case "11":
+                break;
+            case "12":
+                break;
+            case "13":
+                break;
+            case "14":
+                break;
+            case "15":
+                break;
+            default:
+                System.out.println("Invalid option, please enter a number between 1-15!");
         }
-        System.out.println("fk");
+        scanner.close();
+        System.out.println("**********Exit**********");
     }
 }

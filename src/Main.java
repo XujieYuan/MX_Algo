1f3e4b8a601f001c9557781424ea2934d3cf1d0b
Xujie Yuan <xyua0019@student.monash.edu>
2021-10-11 17:15:28 +0800

    2021.10.11

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,32 +6,17 @@ public class Main {
         // System.out.println("Enter the class number:");
         // start();
         // System.out.println("**********Exit**********");
-        int length = 12;
-        // 声明二维数组
-        int[][] arr = new int[length][];
-        // 遍历二维数组
-        for (int i = 0; i < arr.length; i++) {
-            // 打印空格
-            for (int m = 0; m < arr.length - 1 - i; m++) {
-                System.out.print("  ");
-            }
-            // 给每个二维数据的元素赋值一维数组
-            arr[i] = new int[i + 1];
-            // 遍历一维数组
-            for (int j = 0; j < arr[i].length; j++) {
-                // 第一个元素和最后一个元素的值都是1
-                if (j == 0 || j == arr[i].length - 1) {
-                    arr[i][j] = 1;
-                } else {
-                    // 当前一维数组的索引n元素的值，等于前一个数组索引n-1，加上索引n的值
-                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-                }
-                // 格式化输出元素值
-                System.out.printf("%4d", arr[i][j]);
-            }
-            // 换行
-            System.out.println();
+        int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int n = 5;
+        int tmp[] = new int[n];
+        for (int i = 0; i < n; ++i) {
+            tmp[i] = a[n - i - 1];
         }
+        for (int i = 0; i < n; ++i) {
+            a[i] = tmp[i];
+        }
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(tmp));
     }
 
     public static void start() {
